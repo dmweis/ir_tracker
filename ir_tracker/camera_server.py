9f3ccfb5144224118ff62abc5d219cd95822fb72
David Weis <dweis7@gmail.com>
2021-09-28 01:59:24 +0100

    Add debug server lib

---
+++ ir_tracker/camera_server.py
@@ -0,0 +1,19 @@
+from ir_tracker import debug_server
+
+RESOLUTION_WIDTH = 1280
+RESOLUTION_HEIGHT = 720
+FRAMERATE = 30
+RESOLUTION = (RESOLUTION_WIDTH, RESOLUTION_HEIGHT)
+
+if __name__ == "__main__":
+    debug_image_container = debug_server.create_image_server()
+
+    with picamera.PiCamera() as camera:
+        camera.resolution = RESOLUTION
+        camera.framerate = FRAMERATE
+        while True:
+            image = np.empty((RESOLUTION_HEIGHT * RESOLUTION_WIDTH * 3, ),
+                             dtype=np.uint8)
+            camera.capture(image, 'bgr')
+            image = image.reshape((RESOLUTION_HEIGHT, RESOLUTION_WIDTH, 3))
+            debug_image_container["last_image"] = image
---
+++ ir_tracker/debug_server.py
@@ -0,0 +1,67 @@
+import threading
+
+import cv2
+import numpy as np
+import picamera
+from flask import Flask, Response, render_template_string
+
+app = Flask(__name__)
+
+image_container = {}
+
+
+def camera_stream(image_name):
+    while True:
+        _, payload = cv2.imencode('.jpg', image_container[image_name])
+        frame = payload.tobytes()
+        yield (b'--frame/r/n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+INDEX_TEMPLATE = """
+<body>
+    <ul>
+    {% for image_stream in image_stream_list %}
+        <li>
+            <a href="{{url_for('stream_page', image_name=image_stream)}}">{{image_stream}}</a>
+        </li>
+    {% endfor %}
+    </ul>
+</body>
+"""
+
+STREAM_PAGE_TEMPLATE = """
+<body>
+    <img src="{{url_for('video_feed', image_name=image_name)}}" width="100%">
+</body>
+"""
+
+
+@app.route('/image_stream/<image_name>')
+def video_feed(image_name):
+    return Response(camera_stream(image_name),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route("/stream_page/<image_name>")
+def stream_page(image_name):
+    return render_template_string(STREAM_PAGE_TEMPLATE, image_name=image_name)
+
+
+@app.route('/')
+def index():
+    return render_template_string(INDEX_TEMPLATE,
+                                  image_stream_list=image_container.keys())
+
+
+def create_image_server():
+    """
+    Create flask image debug server.
+
+    Warning! This is a very hacky flask server. Make sure to only run this once as it uses global stare
+
+    Returns:
+        Dictionary into which you can insert images with string keys
+    """
+    threading.Thread(target=lambda: app.run(host="0.0.0.0", port=8000)).start()
+    return image_container
