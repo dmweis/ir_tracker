e7aca43dc34e87ca31d26448aa91fe68098e0999
David Weis <dweis7@gmail.com>
2021-10-11 21:20:29 +0100

    Remove old programs

--- ir_tracker/picamera_time_test.py
+++
@@ -1,7 +0,0 @@
-from ir_tracker.utils import utility, picam_wrapper
-
-with picam_wrapper.picamera_opencv_video(resolution=(640, 480),
-                                         framerate=32) as video_stream:
-    frame_timer = utility.FramerateCounter()
-    for frame in video_stream:
-        print("Frame time", frame_timer.measure())
