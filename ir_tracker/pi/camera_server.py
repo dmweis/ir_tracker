709f8ddf6bb0db817a40f1eab4bc882bfbdf75f4
David Weis <dweis7@gmail.com>
2021-10-11 22:57:41 +0100

    Remove old camera server

--- ir_tracker/pi/camera_server.py
+++
@@ -1,8 +0,0 @@
-from ir_tracker.utils import debug_server, picam_wrapper
-
-if __name__ == "__main__":
-    debug_image_container = debug_server.create_image_server()
-
-    with picam_wrapper.opencv_picamera() as camera:
-        while True:
-            debug_image_container["last_image"] = camera.get_frame()
