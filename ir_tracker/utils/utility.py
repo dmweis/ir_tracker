876cb1821be2146753cbad44c9621bc3b3a2ae7d
David Weis <dweis7@gmail.com>
2021-10-11 21:15:30 +0100

    Update utils to default to new image server

--- ir_tracker/utils/utility.py
+++ ir_tracker/utils/utility.py
@@ -4,7 +4,8 @@ import cv2
 import numpy as np
 
 
-def request_image(url: str = "http://camerapi2.local:8080/?action=snapshot"):
+def request_image(
+        url: str = "http://camerapi2.local:8000/image_frame/last_image"):
     resp = urlopen(url)
     image = np.asarray(bytearray(resp.read()), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
